c86c2017f102d392dddca07e43a4a5377eb1e1db
George Beshay <georgebeshay101@gmail.com>
2023-04-23 17:06:57 +0200

    SFG created

--- proj-FE/src/app/Components/main-page/main-page.component.ts
+++ proj-FE/src/app/Components/main-page/main-page.component.ts
@@ -13,6 +13,17 @@ export class MainPageComponent {
   private myStage!: Stage;
   private board!: Layer;
   private rhSelected!: Boolean;
+  private currentNodeNumber !: number;
+  private edgesList: number[][] = [];
+  /* 
+    [
+    [ [to, weight], [to, weight], ..],
+    [ [to, weight], [to, weight], ..],
+    [ [to, weight], [to, weight], ..],
+    [ [to, weight], [to, weight], ..],
+    [ [to, weight], [to, weight]], ..,
+   ]
+   */
   // ------------- Separator -------------
   constructor(){
     
@@ -30,6 +41,7 @@ export class MainPageComponent {
     }
     this.board = new Konva.Layer();
     this.myStage.add(this.board);
+    this.currentNodeNumber = 0;
   }
   // ------------- Separator -------------
   switchSection(rhSelected: Boolean){
@@ -46,37 +58,146 @@ export class MainPageComponent {
   }
   // ------------- Separator -------------
   createNode(){
-
-  }
-  // ------------- Separator -------------
-  createEdge(){
-    const a = new Konva.Group({
-      x: 50,
-      y: 50,
-      draggable: true
+    let id = this.currentNodeNumber;
+    this.currentNodeNumber++;
+    var circle = new Konva.Group({
+      x: 30,
+      y: 30,
+      width: 15,
+      height: 15,
+      draggable: true,
     });
-    a.add(
-      new Konva.Arrow({
-        points: [50, 50, 200, 200],
-        fill: 'black',
-        stroke: 'black',
-        strokeWidth: 2,
-      })
-    )
-    a.add(
+    var c = new Konva.Circle({
+      fill: 'gray',
+      radius: 15,
+      stroke: 'black',
+      strokeWidth: 2,
+    });
+    circle.add(c);
+    circle.add(
       new Konva.Text({
-        text: "test",
+        text: String(id),
         fontSize: 18,
-        fontFamily: 'Calibri',
+        x: -5,
+        y: -7.5,
         fill: '#000',
-        width: 70,
-        padding: 10,
         align: 'center',
       })
     );
+    circle.addEventListener('dragend', () => {
+      circle.setDraggable(false);
+    })
+    this.board.add(circle);
+  }
+  // ------------- Separator -------------
+  initEdge(){
+    let x1 = 0;
+    let y1 = 0;
+    let x2 = 0;
+    let y2 = 0;
+    let i = 0;
+    let start: string;
+    let thisExtender = this;
+    this.myStage.on('click', async function (e) {
+      if (i == 0) {
+        let object = e.target;
+        start = object.getParent().getAttr('Children')[1].getAttr('text');
+        console.log(start);
+        object.getParent().setAttr('draggable', false);
+        x1 = thisExtender.myStage.getPointerPosition()!.x;
+        y1 = thisExtender.myStage.getPointerPosition()!.y;
+        i++;
+      } else if (i == 1) {
+        let object = e.target;
+        let terminal = object
+          .getParent()
+          .getAttr('Children')[1]
+          .getAttr('text');
+        console.log(terminal);
+        object.getParent().setAttr('draggable', false);
+        x2 = thisExtender.myStage.getPointerPosition()!.x;
+        y2 = thisExtender.myStage.getPointerPosition()!.y;
+        i++;
+        thisExtender.createEdge(x1, y1, x2, y2, thisExtender.addEdge(start, terminal, '1'));
+      }
+    });
+  }
+  // ------------- Separator -------------
+  createEdge(x1: number, y1: number, x2: number, y2: number, index: number){
+    let thisExtender = this;
+    const a = new Konva.Group({
+      x: 0,
+      y: 0,
+    });
+    a.add(
+      new Konva.Arrow({
+        points: [x1, y1, x2, y2],
+        pointerLength: 10,
+        pointerWidth: 10,
+        fill: 'black',
+        stroke: 'black',
+        strokeWidth: 2,
+        lineCap: 'round',
+        tension: 0.5,
+      })
+    )
+    var textNode = new Konva.Text({
+      text: '1',
+      x: x1 + (x2 - x1) / 2,
+      y: y1 + (y2 - y1) / 2,
+      fontSize: 20,
+    });
+    textNode.on('dblclick dbltap', () => {
+      // create textarea over canvas with absolute position
+
+      // first we need to find position for textarea
+      // how to find it?
+
+      // at first lets find position of text node relative to the stage:
+      var textPosition = textNode.getAbsolutePosition();
+
+      // then lets find position of stage container on the page:
+      var stageBox = this.myStage.container().getBoundingClientRect();
+
+      // so position of textarea will be the sum of positions above:
+      var areaPosition = {
+        x: stageBox.left + textPosition.x,
+        y: stageBox.top + textPosition.y,
+      };
+
+      // create textarea and style it
+      var textarea = document.createElement('input');
+      document.body.appendChild(textarea);
+
+      textarea.value = textNode.text();
+      textarea.style.position = 'absolute';
+      textarea.style.top = areaPosition.y + 'px';
+      textarea.style.left = areaPosition.x + 'px';
+      textarea.style.width = '50px';
+      textarea.style.height = '30px';
+      textarea.style.padding = '5px';
+      textarea.type = 'number';
+      textarea.focus();
+
+      textarea.addEventListener('mouseleave', function (e) {
+          textNode.text(textarea.value);
+          document.body.removeChild(textarea);
+          thisExtender.edgesList[index][2] = Number(textarea.value);
+          console.log(thisExtender.edgesList);
+      });
+    });
+
+    a.add(textNode);
     this.board.add(a);
   }
   // ------------- Separator -------------
-
+  addEdge(from: string, to: string, weight: string){
+    let from_ = Number(from);
+    let to_ = Number(to);
+    let weight_ = Number(weight);
+    this.edgesList.push([from_, to_, weight_]);
+    console.log(this.edgesList);
+    return this.edgesList.length - 1;
+  }  
   // ------------- Separator -------------
 }
