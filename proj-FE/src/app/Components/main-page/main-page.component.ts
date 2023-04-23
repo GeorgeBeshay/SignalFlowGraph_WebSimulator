8c6e4d7b1cfeb0e07adf6858437c6c2e4422da58
George Beshay <georgebeshay101@gmail.com>
2023-04-23 13:57:55 +0200

    Web page initialized

---
+++ proj-FE/src/app/Components/main-page/main-page.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import Konva from 'konva';
+import { Layer } from 'konva/lib/Layer';
+import { Stage } from 'konva/lib/Stage';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent {
+  // ------------- Separator -------------
+  private myStage!: Stage;
+  private board!: Layer;
+  // ------------- Separator -------------
+  createNode(){
+
+  }
+  // ------------- Separator -------------
+  createEdge(){
+
+  }
+  // ------------- Separator -------------
+
+  // ------------- Separator -------------
+}
