a5353fd8fd539a77327af2f02a15ca1665172972
marshelino-maged <78962756+marshelino-maged@users.noreply.github.com>
2023-04-24 16:17:05 +0200

    the signal flow graph intefrace

---
+++ proj/src/main/java/csCourse/proj/SignalFlowIF.java
@@ -0,0 +1,35 @@
+package csCourse.proj;
+
+import org.springframework.data.util.Pair;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public interface SignalFlowIF {
+
+    //get all the forward paths with the gains
+    ArrayList<Pair<String,Double>> forwardPaths();
+
+    //get all the loops in the given graph with the gains
+    ArrayList<Pair<String,Double>> loops();
+
+    /**
+     [
+        2 Non touching:
+                 [(L1L2,G1G2), (L3L4,G3G4), (L5L6,G5G6)]
+        3 Non touching:
+                []
+     ]
+     */
+    ArrayList<ArrayList<Pair<String,Double>>> NonTochingLoops();
+
+    // index[0] is the over all delta
+    ArrayList<Double> delta();
+
+    //calculate the over all transfer function
+    Double transferFunction();
+
+    //return the number of the solutions in the right hand side
+    // if the system is stable return zero
+    int routh();
+}
